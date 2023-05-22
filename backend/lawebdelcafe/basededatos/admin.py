d5a8756a8369a09c99d83a23fd1ea09d02a1da3b
Eduardo Zurbriggen <106617650+Eduzurbriggen14@users.noreply.github.com>
2023-05-21 21:54:42 -0300

    agregando carpeta backend y lo pedido en sprint 2

---
+++ backend/lawebdelcafe/basededatos/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from basededatos.models import Usuario, Categoria, Producto, Carrito, MetodoPago, Prod_carrito, Orden, DetalleOrden
+
+# Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Categoria)
+#admin.site.register(Stock)
+admin.site.register(Producto)
+admin.site.register(Carrito)
+admin.site.register(MetodoPago)
+admin.site.register(Prod_carrito)
+admin.site.register(Orden)
+admin.site.register(DetalleOrden)
