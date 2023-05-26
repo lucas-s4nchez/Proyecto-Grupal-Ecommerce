2b35c52b9f56daab953d602bd7440798eac88e47
José Martín Oviedo <106499760+JosePolvora@users.noreply.github.com>
2023-05-26 13:43:42 -0300

    Update admin.py

--- backend/lawebdelcafe/basededatos/admin.py
+++ backend/lawebdelcafe/basededatos/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from basededatos.models import Usuario, Categoria, Producto, Carrito, MetodoPago, Prod_carrito, Orden, DetalleOrden
+from basededatos.models import Usuario, Categoria, Stock, Producto, Carrito, MetodoPago, Prod_carrito, Orden, DetalleOrden
 
 # Register your models here.
 
