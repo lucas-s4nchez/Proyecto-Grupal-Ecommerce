65c5f880b10b17514434c9e396877d7e89fea15a
Lucas <lucassanchezdl@gmail.com>
2023-06-04 23:17:17 -0300

    configuracion de cors

--- backend/la_web_del_cafe/settings/base.py
+++ backend/la_web_del_cafe/settings/base.py
@@ -31,13 +31,15 @@ LOCAL_APPS = [
 ]
 
 THIRD_APPS = [
+    'corsheaders',
     'rest_framework',
-    'rest_framework.authtoken',
+    'rest_framework.authtoken'
 ]
 
 INSTALLED_APPS = BASE_APPS + LOCAL_APPS + THIRD_APPS
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -87,6 +89,13 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_REQUIRED = False
 TOKEN_EXPIRED_AFTER_SECONDS = 25
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
+CORS_ORIGINS_WHITELIST = [
+    "http://localhost:4200",
+]
 
 
 # Internationalization
