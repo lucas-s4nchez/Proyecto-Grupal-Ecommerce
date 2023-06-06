7c2a1db3b6c572c101cec6db9eb5fc3b0d11d737
Lucas <lucassanchezdl@gmail.com>
2023-06-05 21:52:07 -0300

    registro de usuario con la API REST

--- frontend/src/app/auth/pages/register/register.component.ts
+++ frontend/src/app/auth/pages/register/register.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
-import { FormControl, Validators } from '@angular/forms';
+import {
+  FormControl,
+  Validators,
+  FormGroup,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,22 +13,42 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  email = new FormControl('', [Validators.required, Validators.email]);
-  name = new FormControl('', [Validators.required]);
-  last_name = new FormControl('', [Validators.required]);
-  password = new FormControl('', [
-    Validators.required,
-    Validators.minLength(6),
-    Validators.maxLength(20),
-  ]);
-  constructor(private authService: AuthService) {
-    this.authService.startRegister('', '', '', '').subscribe({
-      next: (data) => {
-        console.log(data);
-      },
-      error: (errors) => {
-        console.log(errors);
-      },
-    });
+  constructor(private authService: AuthService, private fb: FormBuilder) {}
+
+  formLogin = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    name: ['', [Validators.required]],
+    last_name: ['', [Validators.required]],
+    password: [
+      '',
+      [Validators.required, Validators.minLength(6), Validators.maxLength(20)],
+    ],
+  });
+
+  get formLoginState() {
+    return {
+      email: this.formLogin.get('email') as FormControl,
+      name: this.formLogin.get('name') as FormControl,
+      last_name: this.formLogin.get('last_name') as FormControl,
+      password: this.formLogin.get('password') as FormControl,
+    };
+  }
+
+  registerUser() {
+    this.authService
+      .startRegister(
+        this.formLoginState.email.value,
+        this.formLoginState.name.value,
+        this.formLoginState.last_name.value,
+        this.formLoginState.password.value
+      )
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+        },
+        error: (errors) => {
+          console.log(errors);
+        },
+      });
   }
 }
