fec901cf855479d8a33266d9a97cdad8f8c69d09
Danilo <106564033+1Danilo@users.noreply.github.com>
2023-06-19 01:08:44 -0300

    cambios

--- backend/apps/cart/api/urls.py
+++ backend/apps/cart/api/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 
 
 from apps.cart.api.views.cart_views import CartView
-from apps.cart.api.views.cartitem_views import CartItemView
+from apps.cart.api.views.cartItem_views import CartItemView
 
 urlpatterns = [
     path("cart", CartView.as_view(), name="cart"),
     path("cart/items", CartItemView.as_view(), name="cart-items"),
-    path('cart/items/<int:cart_item_id>/', CartItemView.as_view(), name='cart-item-detail'),
+    path('cart/items/<int:cart_item_id>/',
+         CartItemView.as_view(), name='cart-item-detail'),
 ]
--- backend/apps/cart/api/views/cartItem_views.py
+++ backend/apps/cart/api/views/cartItem_views.py
@@ -1,55 +1,59 @@
-from apps.cart.api.serializers.cartItem_serializers import CartItemSerializer,AddCartItemSerializer,UpdateCartItemSerializer
-from apps.cart.models import CartItem,Cart
+from apps.cart.api.serializers.cartItem_serializers import CartItemSerializer, AddCartItemSerializer, UpdateCartItemSerializer
+from apps.cart.models import CartItem, Cart
+
 
 from rest_framework.views import APIView
-
 from rest_framework.response import Response
 
 
 class CartItemView(APIView):
-    model=CartItem
+    model = CartItem
 
     def get(self, request):
-      user = request.user
-      cart = Cart.objects.get(user=user)
-      cart_items = CartItem.objects.filter(cart_id=cart.id, product__state=True)
-      serializer = CartItemSerializer(cart_items, many=True)
-      return Response(serializer.data)
-    
-    def post(self,request):
-      serializer = AddCartItemSerializer(data=request.data, context={'request': request})
-      if serializer.is_valid():
-          cart_item = serializer.save()
-          cart_items = CartItem.objects.filter(cart=cart_item.cart, product__state=True)
-          serializer = CartItemSerializer(cart_items, many=True)
-          return Response(serializer.data)
-      else:
-          return Response(serializer.errors, status=400)
+        user = request.user
+        cart = Cart.objects.get(user=user)
+        cart_items = CartItem.objects.filter(
+            cart_id=cart.id, product__state=True)
+        serializer = CartItemSerializer(cart_items, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AddCartItemSerializer(
+            data=request.data, context={'request': request})
+        if serializer.is_valid():
+            cart_item = serializer.save()
+            cart_items = CartItem.objects.filter(
+                cart=cart_item.cart, product__state=True)
+            serializer = CartItemSerializer(cart_items, many=True)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
 
     def patch(self, request, cart_item_id):
-      user = request.user
+        user = request.user
 
-      try:
-          cart_item = CartItem.objects.get(id=cart_item_id, cart__user=user)
-      except CartItem.DoesNotExist:
-          return Response({'error': 'El producto no existe en el carrito.'}, status=400)
+        try:
+            cart_item = CartItem.objects.get(id=cart_item_id, cart__user=user)
+        except CartItem.DoesNotExist:
+            return Response({'error': 'El producto no existe en el carrito.'}, status=400)
 
-      serializer = UpdateCartItemSerializer(data=request.data)
-      if serializer.is_valid():
-          quantity = serializer.validated_data['quantity']
-          cart_item.quantity = quantity
-          cart_item.save()
-          return Response({'message': 'La cantidad del producto se actualizó correctamente.'})
-      else:
-          return Response(serializer.errors, status=400)
-      
+        serializer = UpdateCartItemSerializer(data=request.data)
+        if serializer.is_valid():
+            quantity = serializer.validated_data['quantity']
+            cart_item.quantity = quantity
+            cart_item.save()
+            return Response({'message': 'La cantidad del producto se actualizó correctamente.'})
+        else:
+            return Response(serializer.errors, status=400)
+
     def delete(self, request, cart_item_id=None):
         user = request.user
 
         # Verificar si se proporciona un cart_item_id
         if cart_item_id:
             try:
-                cart_item = CartItem.objects.get(id=cart_item_id, cart__user=user)
+                cart_item = CartItem.objects.get(
+                    id=cart_item_id, cart__user=user)
             except CartItem.DoesNotExist:
                 return Response({'error': 'El producto no existe en el carrito.'}, status=400)
 
